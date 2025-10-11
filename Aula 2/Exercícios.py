464b5197fcddd0d623bdd4fb0d468815d2e3bfa3
Gustavo-Nascimentoo <gustavoalm1202@gmail.com>
2025-10-11 16:12:45 -0300

    Add files via upload

---
+++ Aula 2/Exercícios.py
@@ -0,0 +1,38 @@
+print('Bem Vindo')
+
+var_boleana = (True)
+print (type(var_boleana))
+
+n1 = float(input('Digite um número decimal: '))
+n2 = float(input('Digite um número decimal: '))
+print(n1 * n2)
+
+n3 = int(input('Digite um número: '))
+n4 = int(input('Digite um número: '))
+print(n3 / n4)
+
+n5 = int(input('Digite um número: '))
+n6 = int(input('Digite um número: '))
+print(n5 - n6)
+
+n7 = int(input('Digite um número: '))
+n8 = int(input('Digite um número: '))
+print(n7 // n8)
+
+n9 = float(input('Digite um número decimal: '))
+n10 = float(input('Digite um número decimal: '))
+n11 = float(input('Digite um número decimal: '))
+n12 = float(input('Digite um número decimal: '))
+print(n9 * n10 * n11 * n12)
+
+numero = int(input('Digite número inteiro: '))
+print('O dobro de', numero , 'é', numero * 2)
+
+print('Seja bem vindo')
+Nome = input("Digite seu nome")
+Ndpizzas = int(input("Quantas pizzas o senhor(a) deseja?: "))
+Tdpedido = (Ndpizzas * 40)
+print (Nome,',' 'O total do pedido ficou', Tdpedido, 'R$')
+
+
+
