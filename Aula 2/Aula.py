81a174e57dd53ff788617ed2fa4490f81362d176
Gustavo-Nascimentoo <gustavoalm1202@gmail.com>
2025-10-11 12:31:16 -0300

    Aula 2

---
+++ Aula 2/Aula.py
@@ -0,0 +1,68 @@
+# n1 = 10
+# n2 = 20
+# print(n1 + n2) #Mais
+# print(n1 - n2) #Menos
+# print(n1 / n2) #Divisão com virgula
+# print(n1 // n2) #Divisão sem virgula
+# print(n1 % n2) #Resto
+# print(n1 ** n2) #Ao Quadrado
+# print(n1,'+',n2,'=', n1+ n2) #Concatenar
+
+# print(n1 > n2) #Maior
+# print(n1 < n2) #Menor
+# print(n1 >= n2) #Maior igual
+# print(n1 <= n2) #Menor igual
+# print(n1 != n2) #Diferente
+# print (n1 == n2) #Igual
+
+# n1 = float(input('Digite um número: '))
+# n2 = float(input('Digite um número: '))
+# print(n1 + n2)
+
+# n1 = float(input('Digite um número: '))
+# n2 = float(input('Digite um número: '))
+# print(n1 - n2)
+
+# n1 = float(input('Digite um número: '))
+# n2 = float(input('Digite um número: '))
+# print(n1 / n2)
+
+# n1 = float(input('Digite um número: '))
+# n2 = float(input('Digite um número: '))
+# print(n1 // n2)
+
+# n1 = float(input('Digite um número: '))
+# n2 = float(input('Digite um número: '))
+# print(n1 ** n2)
+
+#Concatenar
+
+# nome = input('Digite seu nome: ')
+# idade = int(input('Digite sua idade: '))
+# endereco = input('Digite seu endereço: ')
+# curso = input('Digite seu curso: ')
+# salario = float(input('Digite seu salário: '))
+# print('Nome: ', nome)
+# print('Idade:', idade)
+# print('Endereço', endereco)
+# print('Curso', curso)
+# print('Salário', salario)
+
+n1 = int(input('Digite um número: '))
+print ('O quadrado deste número é', n1 ** 2)
+
+nome = input('Digite seu nome :')
+sobrenome = input('Digite seu sobrenome: ')
+print('Seu nome completo é', nome, sobrenome)
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite um número: '))
+print(str(n1),str(n2))
+
+A= 'Python'
+n1= int(input('Digite um número: '))
+print(A, n1)
+
+frase = 'Olá mundo'
+palavra = input('Digite uma palavra: ')
+print(frase, palavra)
